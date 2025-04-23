d53bec2326a0b44e2cc54111e08811b4d4892e5e
YuugouOhno <yuugou.ohno@gmail.com>
2025-04-23 09:30:53 +0900

    20250423

--- ABC/ABC401/B.cpp
+++ ABC/ABC401/B.cpp
@@ -1,1 +1,23 @@
-cd ABC401
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    int error_count= 0;
+    bool is_login = false;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> S;
+        if (S == "login") {
+            is_login = true;
+        } else if (S == "logout") {
+            is_login = false;
+        }
+        if (!is_login && S == "private") {
+            error_count++;
+        }
+    }
+    cout << error_count << endl;
+    return 0;
+}
---
+++ ABC/ABC401/C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    for(int i=0;i<=N;i++){
+        if(i<K){
+            
+        }
+}
